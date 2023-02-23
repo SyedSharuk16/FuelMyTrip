d91a68db5a930ba74938e67d836e7d383a0a660d
s10194023@connect.np.edu.sg <s10194023@connect.np.edu.sg>
2023-02-24 02:48:15 +0800

    Changed the nav bar

---
+++ FuelMyTrip/Pages/fuelprice.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FuelMyTrip.Pages
+{
+    public class fuelpriceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
